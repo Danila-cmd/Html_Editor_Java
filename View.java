cc0dad2c302369006e3f0434c4584c0b62d4c84d
Danila <dvfeduleev@gmail.com>
2021-01-29 17:41:47 +0300

    add DialogWindow

--- View.java
+++ View.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,8 +13,8 @@ public class View extends JFrame implements ActionListener {
     private Controller controller;
 
     private JTabbedPane tabbedPane = new JTabbedPane(); // панель с вкладками
-    private JTextPane htmlTextPane = new JTextPane(); // компоент для визуального редактирования html - 1 вкладка в панели
-    private JEditorPane plainTextPane = new JEditorPane(); // компоент для визуального редактирования html, отображает код html (теги и их содиржимое) - 2 вкладка в панели
+    private JTextPane htmlTextPane = new JTextPane(); // компонент для визуального редактирования html - 1 вкладка в панели
+    private JEditorPane plainTextPane = new JEditorPane(); // компонент для визуального редактирования html, отображает код html (теги и их содиржимое) - 2 вкладка в панели
 
     public Controller getController() {
         return controller;
@@ -27,10 +31,47 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
     }
 
     public void exit() {
         controller.exit();
+    }
+
+    // инициализация меню
+    public void initMenuBar() {
+    }
+
+    // инициализация панели редактора
+    public void initEditor() {
+
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane1 = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", jScrollPane1);
+        JScrollPane jScrollPane2 = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", jScrollPane2);
+
+        Dimension preferredSize = new Dimension(400, 200);
+        tabbedPane.setPreferredSize(preferredSize);
+
+        TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(tabbedPaneChangeListener);
+
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+    }
+
+    // инициализация графического интерфейса
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged() {
+
     }
 
 }
