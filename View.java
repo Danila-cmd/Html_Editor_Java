2a1fc23217f457f72d61b8014af06ad52fa219e6
Danila <dvfeduleev@gmail.com>
2021-02-03 22:36:52 +0300

    add menu with file and other

--- View.java
+++ View.java
@@ -4,6 +4,9 @@ import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +18,16 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane(); // панель с вкладками
     private JTextPane htmlTextPane = new JTextPane(); // компонент для визуального редактирования html - 1 вкладка в панели
     private JEditorPane plainTextPane = new JEditorPane(); // компонент для визуального редактирования html, отображает код html (теги и их содиржимое) - 2 вкладка в панели
+    private UndoManager undoManager = new UndoManager();
+
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
     public Controller getController() {
         return controller;
@@ -42,6 +55,18 @@ public class View extends JFrame implements ActionListener {
 
     // инициализация меню
     public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
+
     }
 
     // инициализация панели редактора
@@ -72,6 +97,14 @@ public class View extends JFrame implements ActionListener {
 
     public void selectedTabChanged() {
 
+    }
+
+    public boolean canRedo() {
+        return false;
+    }
+
+    public boolean canUndo() {
+        return false;
     }
 
 }
